86df460e6bf1c3dc87040c8165731370ae79781e
Radek Beran <radek.beran@etnetera.cz>
2021-05-08 10:54:16 +0200

    Sample antispam quizes

---
+++ sql/insert_antispam_quiz.sql
@@ -0,0 +1,9 @@
+INSERT INTO antispam_quiz ("question", "answer") VALUES ('Sáňkař sáňkuje, lyžař', 'lyžuje');
+INSERT INTO antispam_quiz ("question", "answer") VALUES ('Kolik je čtyři plus dva (slovem)?', 'šest');
+INSERT INTO antispam_quiz ("question", "answer") VALUES ('Kolik je osm plus čtyři (slovem)?', 'dvanáct');
+INSERT INTO antispam_quiz ("question", "answer") VALUES ('Ráno moudřejší', 'večera');
+INSERT INTO antispam_quiz ("question", "answer") VALUES ('Vidíme očima, slyšíme', 'ušima');
+INSERT INTO antispam_quiz ("question", "answer") VALUES ('Nahoře na hlavě nám rostou', 'vlasy');
+INSERT INTO antispam_quiz ("question", "answer") VALUES ('Jaká je barva trávy?', 'zelená');
+INSERT INTO antispam_quiz ("question", "answer") VALUES ('Jaká je barva jasné oblohy?', 'modrá');
+INSERT INTO antispam_quiz ("question", "answer") VALUES ('Kočka chytá (v množném čísle)', 'myši');
