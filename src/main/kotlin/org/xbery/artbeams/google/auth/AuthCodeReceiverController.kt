49bcc2cd496415c5f958b914fa40883a620cf162
Radek Beran <radek.beran@etnetera.cz>
2024-03-02 15:32:42 +0100

    Fixed redirection in AuthCodeReceiverController REST controller

--- src/main/kotlin/org/xbery/artbeams/google/auth/AuthCodeReceiverController.kt
+++ src/main/kotlin/org/xbery/artbeams/google/auth/AuthCodeReceiverController.kt
@@ -8,10 +8,10 @@ import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
-import org.springframework.web.client.RestClient.UriSpec
 import org.xbery.artbeams.common.Urls
 import org.xbery.artbeams.common.controller.BaseController
 import org.xbery.artbeams.common.controller.ControllerComponents
+import java.net.URI
 import java.nio.charset.StandardCharsets
 
 /**
@@ -30,21 +30,20 @@ class AuthCodeReceiverController(private val googleAuth: GoogleApiAuth, common: 
     ): Any {
         try {
             googleAuth.receiveAuthorizationCodeOrError(code, error)
+            val headers = HttpHeaders()
             return if (error != null) {
-                val headers = HttpHeaders()
                 headers.contentType = MediaType("text", "plain", StandardCharsets.UTF_8)
                 ResponseEntity<String>("Token not received: $error", headers, HttpStatus.OK)
             } else if (code != null) {
                 val returnUrl = googleAuth.getReturnUrl()
                 if (returnUrl != null) {
-                    redirect(Urls.urlWithParam(returnUrl, "authorized", "1"))
+                    headers.location = URI.create(Urls.urlWithParam(returnUrl, "authorized", "1"))
+                    return ResponseEntity<String>(headers, HttpStatus.SEE_OTHER)
                 } else {
-                    val headers = HttpHeaders()
                     headers.contentType = MediaType("text", "plain", StandardCharsets.UTF_8)
                     ResponseEntity<String>("OAuth 2.0 Authentication Token Received", headers, HttpStatus.OK)
                 }
             } else {
-                val headers = HttpHeaders()
                 headers.contentType = MediaType("text", "plain", StandardCharsets.UTF_8)
                 ResponseEntity<String>("No Authentication Token or Error Received", headers, HttpStatus.OK)
             }
