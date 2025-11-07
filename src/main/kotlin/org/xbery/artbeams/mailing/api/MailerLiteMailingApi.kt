b3f1c02ddc20514235ddeb37f5217523f16d599d
Radek Beran <radek.beran@etnetera.cz>
2025-11-07 14:44:31 +0100

    Note on Unsubscribe

--- src/main/kotlin/org/xbery/artbeams/mailing/api/MailerLiteMailingApi.kt
+++ src/main/kotlin/org/xbery/artbeams/mailing/api/MailerLiteMailingApi.kt
@@ -24,10 +24,11 @@ class MailerLiteMailingApi(
     private val mailingApiConfig: MailingApiConfig
 ) : AbstractJsonApi(MailingApiConfig.FEATURE_NAME, restTemplate), MailingApi {
 
-    // TBD RBe: React on Unsubscribe from MailerLite subscription group and update state in internal DB.
-    // For now, MailerLite subscription groups are authoritative source of truth
-    // what subscribers are really confirmed/active. This is sufficient solution,
-    // but data synchronization can be further improved.
+    // NOTE: Reaction on Unsubscribe from MailerLite subscription group
+    // is supported by MailerLiteWebhookController.
+    // MailerLite subscription groups are authoritative source of truth
+    // what subscribers are really confirmed/active, because user can unsubscribe
+    // via their links.
 
     override fun subscribeToGroup(email: String, name: String, subscriberGroupId: String, ipAddress: String?): MailerLiteSubscriptionResponse {
         // TBD RBe: More robust subscription so even if the subscriber already exists in MailerLite,
