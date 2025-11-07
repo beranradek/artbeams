faec31f62f4f4898b0f8cff14df08c6308bea43b
Radek Beran <radek.beran@etnetera.cz>
2025-11-07 13:39:49 +0100

    Fixed user access unmapper to DB record

--- src/main/kotlin/org/xbery/artbeams/common/access/repository/mapper/UserAccessUnmapper.kt
+++ src/main/kotlin/org/xbery/artbeams/common/access/repository/mapper/UserAccessUnmapper.kt
@@ -2,9 +2,11 @@ package org.xbery.artbeams.common.access.repository.mapper
 
 import org.jooq.RecordUnmapper
 import org.springframework.stereotype.Component
+import org.xbery.artbeams.common.Dates
 import org.xbery.artbeams.common.access.domain.UserAccess
 import org.xbery.artbeams.jooq.schema.tables.records.UserAccessRecord
 import org.xbery.artbeams.jooq.schema.tables.references.USER_ACCESS
+import kotlinx.datetime.toJavaZoneId
 
 /**
  * @author Radek Beran
@@ -16,6 +18,7 @@ class UserAccessUnmapper : RecordUnmapper<UserAccess, UserAccessRecord> {
         val record = USER_ACCESS.newRecord()
         record.id = access.id
         record.accessTime = access.time
+        record.accessDate = access.time.atZone(Dates.DEFAULT_TIME_ZONE.toJavaZoneId()).toLocalDate()
         record.ip = access.ip
         record.userAgent = access.userAgent
         record.entityType = access.entityKey.entityType
