e493454e28a614e1690b171520715cdfd6768a89
Radek Beran <beran.radek@seznam.cz>
2026-05-17 18:01:29 +0000

    Remove obsolete add_articles_is_draft migration

--- src/main/resources/sql/migrations/add_articles_is_draft.sql
+++
@@ -1,8 +0,0 @@
--- Add draft flag to articles.
--- Draft articles must never be visible on public web pages.
-
-ALTER TABLE articles
-    ADD COLUMN IF NOT EXISTS is_draft boolean NOT NULL DEFAULT FALSE;
-
-CREATE INDEX IF NOT EXISTS idx_articles_is_draft ON articles (is_draft);
-
